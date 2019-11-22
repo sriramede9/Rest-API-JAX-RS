a6f9590d581a011942b31b4fa6631cdd65fda110
sriramede9 <only4programming03@gmail.com>
2019-11-22 16:58:05 -0500

    added #Response #absolutePath #statusCodes #responsecodes for Messages

--- src/main/java/com/sri/rest/Jax_Rs/resources/MessageResource.java
+++ src/main/java/com/sri/rest/Jax_Rs/resources/MessageResource.java
@@ -1,5 +1,7 @@
 package com.sri.rest.Jax_Rs.resources;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.ws.rs.BeanParam;
@@ -11,7 +13,12 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
 
 import com.sri.rest.Jax_Rs.model.Message;
 import com.sri.rest.Jax_Rs.service.MessageService;
@@ -55,39 +62,72 @@ public class MessageResource {
 	@GET
 	@Path(value = "{id}")
 	@Produces(MediaType.APPLICATION_XML)
-	public Message getMessagebyId(@PathParam("id") long id) {
+	public Response getMessagebyId(@PathParam("id") long id) {
 
-		return ms.getMessage(id);
+		Message message = ms.getMessage(id);
+
+		return Response.accepted(message).build();
+
+		// return ms.getMessage(id);
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_XML)
 	@Produces(MediaType.APPLICATION_XML)
-	public Message addMessage(Message m) {
-		return ms.addMessage(m);
+	public Response addMessage(Message m, @Context UriInfo uriInfo) {
+
+		Message addMessage = ms.addMessage(m);
+
+//		return Response.status(Status.CREATED)
+//			.entity(ms.addMessage(m)).build();
+
+//		return Response.created(new URI("http://localhost:8081/Jax-Rs/webapi/messages/" + addMessage.getId()))
+//				.entity(addMessage).build();
+
+		String path = String.valueOf(addMessage.getId());
+
+		URI uri = uriInfo.getAbsolutePathBuilder().path(path).build();
+
+		return Response.created(uri).entity(addMessage).build();
+
+		// return ms.addMessage(m);
 	}
 
 	@PUT
 	@Consumes(MediaType.APPLICATION_XML)
 	@Produces(MediaType.APPLICATION_XML)
-	public Message updateMessage(Message m) {
+	public Response updateMessage(Message m, @Context UriInfo uriInfo) {
 
-		return ms.updateMessage(m);
+		Message mm = ms.updateMessage(m);
+
+		String path = String.valueOf(mm.getId());
+
+		URI uri = uriInfo.getAbsolutePathBuilder().path(path).build();
+
+		return Response.ok(mm).contentLocation(uri).build();
+
+		// Response.created(uri).entity(mm).build();
+
+		// return ms.updateMessage(m);
 
 	}
 
 	@DELETE
 	@Path(value = "{id}")
 	@Produces(MediaType.APPLICATION_XML)
-	public Message deleteMessage(@PathParam("id") long id) {
+	public Response deleteMessage(@PathParam("id") long id) {
 
-		return ms.removeMessage(id);
+		// Response.
+
+		Message removeMessage = ms.removeMessage(id);
+
+		return Response.ok(removeMessage).build();
 
 	}
 
-	//@GET
+	// @GET
 	@Path(value = "/{mId}/comments")
-	 @Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_XML)
 	public CommentResource getCommentResource() {
 		return new CommentResource();
 	}
